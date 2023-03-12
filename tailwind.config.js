2f7f5727b2c488f91b8470eec34156a2f43f2508
Luke Bouch <luke@lukebouch.com>
2023-03-12 16:12:15 -0400

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-    purge: {
-        content: [
-            "source/**/*.html",
-            "source/**/*.md",
-            "source/**/*.js",
-            "source/**/*.php",
-            "source/**/*.vue",
-        ],
-        options: {
-            whitelist: [/language/, /hljs/, /mce/],
-        },
-    },
+    content: [
+        "source/**/*.html",
+        "source/**/*.md",
+        "source/**/*.js",
+        "source/**/*.php",
+        "source/**/*.vue",
+    ],
     plugins: [require("@tailwindcss/forms")],
     theme: {
         extend: {
